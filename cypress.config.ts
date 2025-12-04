da62b1b42dcb92341de90c9c4baa1d64059dd9ec
vadym_beetroot <vadym.vyshnevskiy@beetroot.se>
2025-12-05 00:25:29 +0100

    fix 4

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     projectId: "rb8f1i",
-    experimentalStudio: true,
     video: false,
     screenshotOnRunFailure: false,
     experimentalPromptCommand: true,
